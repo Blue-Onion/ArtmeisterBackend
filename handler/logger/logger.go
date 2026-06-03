d73b5d9c361b4e4f51642d1fcc2349221a65a6f4
Blue-Onion <rawatadityasingh963@gmail.com>
2026-06-03 22:28:50 +0530

    Logger Init

--- handler/logger/logger.go
+++ handler/logger/logger.go
@@ -14,15 +14,16 @@ type Logger struct {
 }
 
 var (
-	log  *Logger
-	once sync.Once
+	log    *Logger
+	once   sync.Once
+	logErr error
 )
 
-func GetLogger() *Logger {
+func GetLogger() (*Logger, error) {
 	once.Do(func() {
-		log = loadLogger(appName)
+		log, logErr = loadLogger(appName)
 	})
-	return log
+	return log, logErr
 }
 func isPath(path string) bool {
 	_, err := os.Stat(path)
@@ -32,18 +33,51 @@ func isPath(path string) bool {
 	return false
 
 }
-func loadLogger(str string) *Logger {
+func createFile(filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return nil
+}
+func initLogsFile() error {
+	files := []string{
+		"err.log",
+		"info.log",
+	}
+	for _, f := range files {
+		filePath := fmt.Sprintf("%s/%s", path, f)
+		_, err := os.Stat(filePath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				err = createFile(filePath)
+				if err != nil {
+					return err
+				}
+				return nil
+			} else {
+				return err
+			}
+		}
+	}
+	return nil
+}
+func loadLogger(str string) (*Logger, error) {
 	ok := isPath(path)
 	if !ok {
 		err := os.Mkdir(path, os.ModePerm)
 		if err != nil {
-			fmt.Println(err.Error())
-			return nil
+			return nil, err
 		}
+	}
+	err := initLogsFile()
+	if err != nil {
+		return nil, err
 	}
 	return &Logger{
 		service: str,
-	}
+	}, nil
 }
 func (*Logger) Info(str string) {
 
